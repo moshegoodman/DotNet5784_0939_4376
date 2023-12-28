b004b144b080e0c8e68b48cd6ca19abca3ec0887
moshegoodman <moshegoodman7@gmail.com>
2023-12-28 13:41:09 +0200

    look at what i did!!!!!!

---
+++ Stage0/Program0939.cs
@@ -0,0 +1,10 @@
+﻿namespace Targil0
+{
+    partial class program
+    {
+        static partial void Welcome0939()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
