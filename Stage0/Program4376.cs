4216ec985784a6ae6d98619cd0f2d9053dba993d
דוד פורסט <dforst@g.jct.ac.il>
2023-12-28 13:00:51 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program4376.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4376();
+            Welcome0939();
+            Console.ReadKey();
+        }
+        static partial void Welcome0939();
+
+        private static void Welcome4376()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userInput);
+        }
+
+    }
+}
+
