68d04a4f0c4d8d4523cc15be53f2d857ba2e4f01
moshegoodman <moshegoodman7@gmail.com>
2024-01-07 12:19:20 +0200

    engineer final touches

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,14 +1,21 @@
 ﻿namespace DO;
-
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id">A unique number for each engineer</param>
+/// <param name="Email">the engineers email</param>
+/// <param name="Cost">the engineers salary per hour</param>
+/// <param name="Name">engineers full name</param>
+/// <param name="Level">engineers level</param>
 public record Engineer
 (
     int Id,
-    string Email,
-    double Cost,
-    string Name,
-    DO.EngineerExperience Level
+    string? Email = null,
+    double? Cost = null,
+    string? Name = null,
+    DO.EngineerExperience? Level = null
 )
-{ 
+{
+    Engineer() : this(0) { }
 
 }
- 
