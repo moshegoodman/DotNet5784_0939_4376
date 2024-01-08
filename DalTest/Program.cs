54ab5042ef87f80bb1129673db23a75270bc04cf
moshegoodman <moshegoodman7@gmail.com>
2024-01-08 15:31:25 +0200

    dependecy completed

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,29 +1,11 @@
 ﻿namespace DalTest;
 using Dal;
 using DalApi;
+using DO;
+
 internal class Program
 {
-    private void MenuTasks()
-    {
 
-        Console.WriteLine("Enter 1 to exit the main menu");
-        Console.WriteLine("Enter 2 to add a new task ");
-        Console.WriteLine("Enter 3 to read a task ");
-        Console.WriteLine("Enter 4 to read all tasks ");
-        Console.WriteLine("Enter 5 to update a task ");
-        Console.WriteLine("Enter 6 to delete a task");
-        string a = Console.ReadLine()!;
-        do
-            switch (a)
-            {
-                case "2": TaskCreate();
-                case "3": TaskRead();
-                case "4": TaskReadAll();
-                case "5": TaskUpdate();
-                case "6": TaskDelete();
-            }
-        while (a != "1");
-    }
     private DateTime? GetDateTime()
     {
         Console.WriteLine("Enter the year:");
@@ -44,64 +26,45 @@ internal class Program
             return null;
         }
     }
+    private void TaskMenu()
+    {
+        Console.WriteLine("Enter the alias of the task:");
+        string _alias = Console.ReadLine()!;
+        Console.WriteLine("Enter 1 to exit the task menu");
+        Console.WriteLine("Enter 2 to add a new task ");
+        Console.WriteLine("Enter 3 to read a task ");
+        Console.WriteLine("Enter 4 to read all tasks ");
+        Console.WriteLine("Enter 5 to update a task ");
+        Console.WriteLine("Enter 6 to delete a task");
+        string a = Console.ReadLine()!;
+        do
+            switch (a)
+            {
+                case "2":
+                    TaskCreate();
+                    break;
+                case "3":
+                    TaskRead();
+                    break;
+
+                case "4":
+                    TaskReadAll();
+                    break;
+                case "5":
+                    TaskUpdate();
+                    break;
+                case "6":
+                    TaskDelete();
+                    break;
+
+            }
+        while (a != "1");
+    }
+
     private void TaskCreate()
     {
         Console.WriteLine("Enter the alias of the task:");
         string _alias = Console.ReadLine()!;
-            Console.WriteLine("Enter 1 to exit the task menu");
-            Console.WriteLine("Enter 2 to add a new task ");
-            Console.WriteLine("Enter 3 to read a task ");
-            Console.WriteLine("Enter 4 to read all tasks ");
-            Console.WriteLine("Enter 5 to update a task ");
-            Console.WriteLine("Enter 6 to delete a task");
-            string a = Console.ReadLine()!;
-            do
-                switch (a)
-                {
-                    case "2":
-                        TaskCreate();
-                        break;
-                    case "3":
-                        TaskRead();
-                        break;
-
-                    case "4":
-                        TaskReadAll();
-                        break;
-                    case "5":
-                        TaskUpdate();
-                        break;
-                    case "6":
-                        TaskDelete();
-                        break;
-
-                }
-            while (a != "1");
-        }
-        private DateTime? GetDateTime()
-        {
-            Console.WriteLine("Enter the year:");
-            int year = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the month:");
-            int month = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter th day:");
-            int day = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the hour:");
-            int hour = Convert.ToInt32(Console.ReadLine());
-            try
-            {
-                return new DateTime(year, month, day, hour, 0, 0);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid date or time entered.");
-                return null;
-            }
-        }
-        private void TaskCreate()
-        {
-            Console.WriteLine("Enter the alias of the task:");
-            string _alias = Console.ReadLine()!;
 
         Console.WriteLine("Enter a description of the task:");
         string _description = Console.ReadLine()!;
@@ -147,100 +110,125 @@ internal class Program
 
 
 
-        private void MenuDependents()
-        {
-            Console.WriteLine("Enter 1 to exit the dependency menu");
-            Console.WriteLine("Enter 2 to add a new dependency ");
-            Console.WriteLine("Enter 3 to read a dependency ");
-            Console.WriteLine("Enter 4 to read all dependencies ");
-            Console.WriteLine("Enter 5 to update a dependency ");
-            Console.WriteLine("Enter 6 to delete a dependency");
-            string a = Console.ReadLine()!;
-            do
-                switch (a)
-                {
-                    case "2":
-                        DependencyCreate();
-                        break;
-                    case "3":
-                        DependencyRead();
-                        break;
-
-                    case "4":
-                        DependencyReadAll();
-                        break;
-                    case "5":
-                        DependencyUpdate();
-                        break;
-                    case "6":
-                        DependencyDelete();
-                        break;
-
-                }
-            while (a != "1");
-        }
-
-
-        private void DependencyCreate()
-        {
-            Console.WriteLine("Enter the dependant task:");
-            int dependant_task = Convert.ToInt32(Console.ReadLine())!;
-
-            Console.WriteLine("Enter the dependamt on tasktask:");
-            int dependamt_on_tasktask = Convert.ToInt32(Console.ReadLine())!;
-
-
-            DO.Dependency newDependency = new(0, dependant_task, dependamt_on_tasktask);
-            DependencyImplementation? copyDependency = null;
-            copyDependency!.Create(newDependency);
-        }
-        private void DependencyRead()
-        {
-            Console.WriteLine("Enter the dependancy id:");
-            int _dependancy = Convert.ToInt32(Console.ReadLine())!;
-
-            DO.Dependency? _dependency = null;
-            DependencyImplementation? readDependency = null;
-
-            Console.WriteLine(readDependency!.Read(_dependancy));
-        }
-        private void DependencyReadAll()
-        {
-            List<DO.Dependency>? _allDependencies = null;
-            DependencyImplementation? readDependency = null;
-
-            _allDependencies = readDependency!.ReadAll();
-            foreach (DO.Dependency dependency in _allDependencies)
+    private void MenuDependents()
+    {
+        Console.WriteLine("Enter 1 to exit the dependency menu");
+        Console.WriteLine("Enter 2 to add a new dependency ");
+        Console.WriteLine("Enter 3 to read a dependency ");
+        Console.WriteLine("Enter 4 to read all dependencies ");
+        Console.WriteLine("Enter 5 to update a dependency ");
+        Console.WriteLine("Enter 6 to delete a dependency");
+        string a = Console.ReadLine()!;
+        do
+            switch (a)
             {
-                Console.WriteLine(dependency);
+                case "2":
+                    DependencyCreate();
+                    break;
+                case "3":
+                    DependencyRead();
+                    break;
+
+                case "4":
+                    DependencyReadAll();
+                    break;
+                case "5":
+                    DependencyUpdate();
+                    break;
+                case "6":
+                    DependencyDelete();
+                    break;
+
             }
-        }
-        private void DependencyUpdate()
+        while (a != "1");
+    }
+
+
+    private void DependencyCreate()
+    {
+        Console.WriteLine("Enter the dependant task:");
+        int dependant_task = Convert.ToInt32(Console.ReadLine())!;
+
+        Console.WriteLine("Enter the dependamt on tasktask:");
+        int dependamt_on_tasktask = Convert.ToInt32(Console.ReadLine())!;
+
+
+        DO.Dependency newDependency = new(0, dependant_task, dependamt_on_tasktask);
+        DependencyImplementation? copyDependency = null;
+        copyDependency!.Create(newDependency);
+    }
+    private void DependencyRead()
+    {
+        Console.WriteLine("Enter the dependancy id:");
+        int _dependancy = Convert.ToInt32(Console.ReadLine())!;
+
+        DependencyImplementation? readDependency = null;
+
+        Console.WriteLine(readDependency!.Read(_dependancy));
+    }
+    private void DependencyReadAll()
+    {
+        List<DO.Dependency>? _allDependencies = null;
+        DependencyImplementation? readDependency = null;
+
+        _allDependencies = readDependency!.ReadAll();
+        foreach (DO.Dependency dependency in _allDependencies)
         {
-            Console.WriteLine("Enter the dependancy id:");
-            int _dependancy = Convert.ToInt32(Console.ReadLine())!;
+            Console.WriteLine(dependency);
         }
-        private void DependencyDelete()
-        {
+    }
+    private void DependencyUpdate()
+    {
+        Console.WriteLine("Enter the dependancy id:");
+        int _dependancyId = Convert.ToInt32(Console.ReadLine())!;
+        Console.WriteLine("Enter the dependant task:");
+        int dependant_task = Convert.ToInt32(Console.ReadLine())!;
 
-        }
-
-
-
-
-
-
-
-
+        Console.WriteLine("Enter the dependamt on tasktask:");
+        int dependamt_on_tasktask = Convert.ToInt32(Console.ReadLine())!;
 
 
-
+        Dependency updatedDependency = new Dependency(_dependancyId, dependant_task, dependamt_on_tasktask);
 
 
 
+        DependencyImplementation? readDependency = null;
 
-        private void MenuEngineers()
-        {
+        if (readDependency!.Read(_dependancyId) == updatedDependency)
+            return;
+
+        readDependency!.Delete(_dependancyId);
+        readDependency!.Create(updatedDependency);
+
+
+
+
+        IDependency? _dependency = null;
+    }
+    private void DependencyDelete()
+    {
+        Console.WriteLine("Enter the dependancy id:");
+        int _dependancy = Convert.ToInt32(Console.ReadLine())!;
+        IDependency? _dependency = null;
+        _dependency!.Delete(_dependancy);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void MenuEngineers()
+    {
 
     }
     private static ITask? s_dalTask = new TaskImplementation();
