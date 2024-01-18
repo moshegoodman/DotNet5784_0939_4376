8531019e5124d116c0f7ff6795886ec1ee410511
moshegoodman <moshegoodman7@gmail.com>
2024-01-18 12:38:01 +0200

    updates taskImplenentation

--- DalXml/EngineerImplementation.cs
+++ DalXml/EngineerImplementation.cs
@@ -10,33 +10,74 @@ internal class EngineerImplementation : IEngineer
 {
     readonly string s_engineers_xml = "engineers";
 
+    //creates an engineer occurance
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
+
+        if (Read(item.Id) is not null)
+        {
+            throw new DalAlreadyExistsException($"Engineer with ID={item.Id} already exists");
+        }
+        engineers.Add(item);
+        XMLTools.SaveListToXMLSerializer(engineers, s_engineers_xml);
+
+        return item.Id;
     }
 
+    //deletes a given occurrence
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
+
+        if (Read(id) is null)
+            throw new DalDoesNotExistException($"Engineer with ID={id} does not exists");
+        engineers.Remove(Read(id)!);
+        XMLTools.SaveListToXMLSerializer(engineers, s_engineers_xml);
+
     }
 
+    //prints all fields of a given engineer occurrence
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
+
+        return engineers.FirstOrDefault(item => item.Id == id);
     }
 
-    public Engineer? Read(Func<Engineer, bool> filter)
+    //prints all fields of all ocurrences by condition
+    public IEnumerable<Engineer> ReadAll(Func<Engineer, bool>? filter = null)
     {
-        throw new NotImplementedException();
-    }
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
 
-    public IEnumerable<Engineer?> ReadAll(Func<Engineer, bool>? filter = null)
-    {
-        throw new NotImplementedException();
+        if (filter != null)
+        {
+            return from item in engineers
+                   where filter(item)
+                   select item;
+        }
+        return from item in engineers
+               select item;
     }
-
+    //updates an occurrence (the user enters vulues of all fields)
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
+
+        if (Read(item.Id) is null)
+            throw new DalDoesNotExistException($"Engineer with ID={item.Id} does not exists");
+        Delete(item.Id);
+        engineers.Add(item);
+        XMLTools.SaveListToXMLSerializer(engineers, s_engineers_xml);
+
     }
+    //Reads entity object by a given condition
+    public Engineer? Read(Func<Engineer, bool> filter)
+    {
+        List<Engineer> engineers = XMLTools.LoadListFromXMLSerializer<Engineer>(s_engineers_xml);
+
+        return engineers.FirstOrDefault(item => filter(item));
+
+    }
+
 }
--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -12,31 +12,68 @@ internal class TaskImplementation : ITask
 
     public int Create(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+
+        int id = Config.NextTaskId;
+        Task newItem = item with { Id = id };
+        tasks.Add(newItem);
+        XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
+
+        return id;
     }
 
+    //deletes a given task occurrence
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+
+        if (Read(id) is null)
+            throw new DalDoesNotExistException($"Task with ID={id} does not exists");
+        tasks.Remove(Read(id)!);
+        XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
+
     }
 
+    //prints all fields of a given task occurrence
     public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+
+        return tasks.FirstOrDefault(item => item.Id == id);
+
     }
 
-    public Task? Read(Func<Task, bool> filter)
+    //prints all fields of all ocurrences 
+    public IEnumerable<Task> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
-    }
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
 
-    public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
-    {
-        throw new NotImplementedException();
+        if (filter != null)
+        {
+            return from item in tasks
+                   where filter(item)
+                   select item;
+        }
+        return from item in tasks
+               select item;
     }
-
+    //updates an occurrence (the user enters vulues of all fields)
     public void Update(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+
+        if (Read(item.Id) is null)
+            throw new DalDoesNotExistException($"Task with ID={item.Id} does not exists");
+        Delete(item.Id);
+        tasks.Add(item);
+        XMLTools.SaveListToXMLSerializer(tasks, s_tasks_xml);
+
+    }
+    //Reads entity object by a given condition
+    public Task? Read(Func<Task, bool> filter)
+    {
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);
+
+        return tasks.FirstOrDefault(item => filter(item));
     }
 }
