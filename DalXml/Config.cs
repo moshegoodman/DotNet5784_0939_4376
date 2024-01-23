1d306fbf73e68441b57e7dcb9677594618cb9cf4
moshegoodman <moshegoodman7@gmail.com>
2024-01-23 13:07:35 +0200

    asd

--- DalXml/Config.cs
+++ DalXml/Config.cs
@@ -5,4 +5,8 @@ internal static class Config
     static string s_data_config_xml = "data-config";
     internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
     internal static int NextDependencyId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); }
+    internal static int ResetDependencyId { get => XMLTools.ResetDependencyId(s_data_config_xml, "NextDependencyId"); }
+
+    internal static int ResetTaskId { get => XMLTools.ResetTaskId(s_data_config_xml, "NextTaskId"); }
+
 }
